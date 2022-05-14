bbb2c4bf6afb671a39b64fc01b261c66f8ddb4e7
Burak Karataş <93159179+BlackBrandr@users.noreply.github.com>
2022-05-14 14:51:59 +0300

    v3
    Tryin new navbar for navigation

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import Login from './components/LogIn'
 import BlackList from './components/BlackList'
 import AccountBlacklist from './components/AccountBlacklist'
 import TxBlacklist from './components/TxBlacklist'
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 <script src="../path/to/flowbite/dist/flowbite.js"></script>
 
 
 function App() {
-  return (
-    <>    
-         <AccountBlacklist />
-    </>
-  );
+  
+    return (      
+     <>
+     <AccountBlacklist/>
+     </>
+    );
+  
 }
 
 export default App;
