ca12a96753dfdf5b14cf0301cf70847771474824
Burak Karataş <93159179+BlackBrandr@users.noreply.github.com>
2022-05-14 09:21:58 +0300

    v2.1
    Before Dynamic Route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import Login from './components/LogIn'
 import BlackList from './components/BlackList'
+import AccountBlacklist from './components/AccountBlacklist'
 import TxBlacklist from './components/TxBlacklist'
-
-
-
 <script src="../path/to/flowbite/dist/flowbite.js"></script>
 
 
 function App() {
   return (
     <>    
-         <TxBlacklist />
+         <AccountBlacklist />
     </>
   );
 }
